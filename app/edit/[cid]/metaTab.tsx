50911560057c0f19b872e63406a0e4c9b0c01be8
na-trium-144 <100704180+na-trium-144@users.noreply.github.com>
2024-10-12 02:33:12 +0900

    edit時のsessionIdを固定

--- app/edit/[cid]/metaTab.tsx
+++ app/edit/[cid]/metaTab.tsx
@@ -91,6 +91,8 @@ export function MetaEdit(props: Props) {
 }
 
 interface Props2 {
+  sessionId?: number;
+  setSessionId: (id: number) => void;
   chart?: Chart;
   setChart: (chart: Chart) => void;
   cid: string | undefined;
@@ -193,7 +195,6 @@ export function MetaTab(props: Props2) {
     }
   };
 
-  const [sessionId, setSessionId] = useState<number>();
   const [sessionData, setSessionData] = useState<SessionData>();
   useEffect(() => {
     if (props.chart) {
@@ -204,25 +205,25 @@ export function MetaTab(props: Props2) {
         chart: props.chart,
       };
       setSessionData(data);
-      if (sessionId === undefined) {
-        setSessionId(initSession(data));
+      if (props.sessionId === undefined) {
+        props.setSessionId(initSession(data));
       } else {
-        initSession(data, sessionId);
+        initSession(data, props.sessionId);
       }
     }
-  }, [sessionId, props]);
+  }, [props]);
 
   return (
     <>
           <p className="mb-1">
             <a
               className="hover:text-blue-600 underline relative inline-block"
-              href={`/play?sid=${sessionId}`}
+              href={`/play?sid=${props.sessionId}`}
               target="_blank"
             >
               <button
                 onClick={() =>
-                  sessionData && initSession(sessionData, sessionId)
+                  sessionData && initSession(sessionData, props.sessionId)
                 }
               >
                 <span className="mr-5">テストプレイ</span>
