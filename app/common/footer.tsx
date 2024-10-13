4c60df231912f2be62f99bc240cf8afbd2f34475
na-trium-144 <100704180+na-trium-144@users.noreply.github.com>
2024-10-14 00:20:29 +0900

    footerの注意書き削除

--- app/common/footer.tsx
+++ app/common/footer.tsx
@@ -48,10 +48,6 @@ export default function Footer(props: Props) {
           <span>({process.env.buildCommit})</span>
         </div>
       </div>
-      <p className="my-1 text-center text-sm">
-        ※ FallingNikochanは開発中です。
-        予告なく仕様変更したりサーバーダウンしたりデータが消えたりする可能性があります。
-      </p>
     </footer>
   );
 }
