7425a4ade5ff199ca5cd81f633fc521fde8d4a1c
k. Naka <100704180+na-trium-144@users.noreply.github.com>
2024-10-16 14:42:13 +0900

    データを圧縮して保存 (#78)

--- app/api/fsAccess.ts
+++ app/api/fsAccess.ts
@@ -1,7 +1,9 @@
 import "dotenv/config";
 
 export async function fsAssign() {
-  const fsRes = await fetch(process.env.FS_MASTER + "/dir/assign", {cache: "no-store"});
+  const fsRes = await fetch(process.env.FS_MASTER + "/dir/assign", {
+    cache: "no-store",
+  });
   const fsResBody = await fsRes.json();
   if (typeof fsResBody.fid === "string") {
     return { fid: fsResBody.fid };
@@ -12,7 +14,10 @@ export async function fsAssign() {
 }
 export async function fsWrite(fid: string, data: Blob) {
   const formData = new FormData();
-  formData.append("file", data);
+  const compressedBlob = await new Response(
+    data.stream().pipeThrough(new CompressionStream("gzip"))
+  ).blob();
+  formData.append("file", compressedBlob);
   const fsPostRes = await fetch(process.env.FS_VOLUME + "/" + fid, {
     method: "POST",
     body: formData,
@@ -26,9 +31,14 @@ export async function fsWrite(fid: string, data: Blob) {
   }
 }
 export async function fsRead(fid: string) {
-  const fsRes = await fetch(process.env.FS_VOLUME + "/" + fid, {cache: "no-store"});
-  if (fsRes.ok) {
-    return { data: await fsRes.arrayBuffer() };
+  const fsRes = await fetch(process.env.FS_VOLUME + "/" + fid, {
+    cache: "no-store",
+  });
+  if (fsRes.ok && fsRes.body) {
+    const decompressedBuf = await new Response(
+      fsRes.body.pipeThrough(new DecompressionStream("gzip"))
+    ).arrayBuffer();
+    return { data: decompressedBuf };
   } else {
     console.log(fsRes);
     return null;
