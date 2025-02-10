d8138ae12ad3fc4fa030692a481d644cd75b3fe0
na-trium-144 <100704180+na-trium-144@users.noreply.github.com>
2025-02-10 15:59:49 +0900

    jsonをjsに変更

--- app/[locale]/getTranslations.ts
+++ app/[locale]/getTranslations.ts
@@ -2,11 +2,11 @@ import { createTranslator } from "next-intl";
 
 export async function getMessages(locale: string) {
   return {
-    ...(await import(`../i18n/${locale}/main.json`)).default,
-    ...(await import(`../i18n/${locale}/about.json`)).default,
-    ...(await import(`../i18n/${locale}/share.json`)).default,
-    ...(await import(`../i18n/${locale}/play.json`)).default,
-    ...(await import(`../i18n/${locale}/edit.json`)).default,
+    ...(await import(`../i18n/${locale}/main.js`)).default,
+    ...(await import(`../i18n/${locale}/about.js`)).default,
+    ...(await import(`../i18n/${locale}/share.js`)).default,
+    ...(await import(`../i18n/${locale}/play.js`)).default,
+    ...(await import(`../i18n/${locale}/edit.js`)).default,
   };
 }
 
---
+++ app/i18n/en/main.js
@@ -0,0 +1,61 @@
+export default {
+  main: {
+    about: {
+      title: "What is Falling Nikochan?",
+    },
+    play: {
+      title: "Play",
+      inputId: "Enter Chart ID",
+      inputIdDesc: "Enter the ID of the chart you want to play.",
+      inputIdDesc2:
+        "You can also play by accessing the chart URL (<url></url>).",
+      inputDirect: "Enter Chart ID and go to play screen directly",
+      inputDirectDevonly: "dev environment only, instead of /share/cid path",
+      recentPlay: "Recently Played Charts",
+      latest: "Latest Charts",
+      latestDesc: "List of charts recently created or updated.",
+      latestDesc2:
+        "For chart creators: Check 'Make public' in chart editor to reflect here after a few minutes.",
+      sample: "Sample Charts",
+      sampleDesc:
+        "Charts created by Falling Nikochan author <small>(na-trium-144)</small>. Start here if you are new. ",
+      sampleDesc2:
+        "Also, more charts are available on Falling Nikochan's YouTube channel <youtube>@nikochan144</youtube>.",
+    },
+    edit: {
+      title: "Chart Editor",
+      welcome:
+        "Welcome to Falling Nikochan chart editor. " +
+        "You can create charts without an account.",
+      inputId: "Enter Chart ID",
+      inputIdDesc:
+        "Enter the ID of the chart you want to edit. " +
+        "Password is required after entering ID.",
+      newTab: "Open in new tab",
+      recentEdit: "Recently Edited Charts",
+      new: "Create New Chart",
+      newButton: "Create",
+      newDesc:
+        "You can save a chart to the server once every {rateLimitMin} minutes." +
+        " (You can overwrite a chart as many times as you want.)",
+    },
+    chartList: {
+      showAll: "Show All",
+      empty: "No charts yet",
+    },
+    policies: {
+      title: "(Kind of) Terms of Use",
+    },
+    version: {
+      title: "Version Info",
+      changelog: "Main Change Logs",
+    },
+  },
+  footer: {
+    history: "ChangeLog here",
+    theme: "Theme",
+    light: "Light",
+    dark: "Dark",
+    default: "System Default",
+  },
+};
