6a3deb43d9458c762b584b67a874b37897a34498
na-trium-144 <100704180+na-trium-144@users.noreply.github.com>
2025-02-22 23:31:44 +0900

    fileMaxSizeを大きめにした

--- chartFormat/apiConfig.ts
+++ chartFormat/apiConfig.ts
@@ -1,6 +1,12 @@
 export const rateLimitMin = 10;
 export const chartMaxEvent = 20000;
-export const fileMaxSize = chartMaxEvent * 100;
+export const fileMaxSize = chartMaxEvent * (100 + 150);
+// luaコードは1コマンドあたり100byteもあれば十分
+// Step: 26 + 27 = 53
+// NoteCommand: 32 + 38 + 53 = 123
+// RestStep: 20 + 9 + 53 * 2 = 135
+// BPMChange: 21 + 27 + 53 = 101
+// Signature: 27 + 30くらい + 53 * 2 = 133
 
 export function validCId(cid: string) {
   return cid.length === 6 && Number(cid) >= 100000 && Number(cid) < 1000000;
