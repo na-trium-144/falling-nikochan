0623cf57b04c033adcbac24146fde3d65bbe5a15
na-trium-144 <100704180+na-trium-144@users.noreply.github.com>
2025-02-27 21:53:14 +0900

    nullが生成された場合エラーにする

--- route/src/og.tsx
+++ route/src/og.tsx
@@ -54,128 +54,137 @@ const ogApp = new Hono<{ Bindings: Bindings }>({ strict: false }).get(
     const briefRes = await pBriefRes;
     if (briefRes.ok) {
       const brief = (await briefRes.json()) as ChartBrief;
-      return c.body(
-        new ImageResponse(
-          (
+      const imRes = new ImageResponse(
+        (
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              position: "absolute",
+              width: "100%",
+              height: "100%",
+              color: "rgb(29 41 61)", // text-slate-800
+            }}
+          >
+            <img
+              style={{
+                width: "100%",
+                position: "absolute",
+              }}
+              src={new URL(
+                `/assets/ogTemplateShare.png`,
+                new URL(c.req.url).origin
+              ).toString()}
+            />
+            <img
+              style={{
+                position: "absolute",
+                top: 0,
+                right: 0,
+                width: 1200 / 3 + 1,
+                objectFit: "cover",
+              }}
+              src={
+                // defaultやhqやsdは4:3で、mqだけなぜか16:9
+                `https://i.ytimg.com/vi/${brief.ytId}/mqdefault.jpg`
+              }
+            />
+            <div
+              style={{
+                paddingLeft: 32 + 96,
+                marginTop: 160,
+                // 5xl
+                fontSize: 48,
+                lineHeight: 1,
+                fontFamily: fontMainUi,
+              }}
+            >
+              {cid}
+            </div>
+            <div
+              style={{
+                paddingLeft: 32,
+                marginTop: 32,
+                width: 2147483647,
+                // 困ったことにellipsisが効かない
+                // width: "100%",
+                // textWrap: "nowrap",
+                // textOverflow: "ellipsis",
+                // overflowX: "clip",
+                // overflowY: "visible",
+                // 7xl
+                fontSize: 72,
+                lineHeight: 1,
+                fontFamily: fontTitle,
+              }}
+            >
+              {brief.title}
+            </div>
+            <div
+              style={{
+                paddingLeft: 32,
+                marginTop: 24,
+                width: 2147483647,
+                // 6xl
+                fontSize: 60,
+                lineHeight: 1,
+                fontFamily: fontTitle,
+              }}
+            >
+              {brief.composer}
+            </div>
             <div
               style={{
                 display: "flex",
-                flexDirection: "column",
-                position: "absolute",
-                width: "100%",
-                height: "100%",
-                color: "rgb(29 41 61)", // text-slate-800
+                alignItems: "baseline",
+                paddingLeft: 32,
+                marginTop: 24,
+                width: 2147483647,
+                // 6xl
+                fontSize: 60,
+                lineHeight: 1,
+                fontFamily: fontTitle,
               }}
             >
-              <img
+              <span
                 style={{
-                  width: "100%",
-                  position: "absolute",
-                }}
-                src={new URL(
-                  `/assets/ogTemplateShare.png`,
-                  new URL(c.req.url).origin
-                ).toString()}
-              />
-              <img
-                style={{
-                  position: "absolute",
-                  top: 0,
-                  right: 0,
-                  width: 1200 / 3 + 1,
-                  objectFit: "cover",
-                }}
-                src={
-                  // defaultやhqやsdは4:3で、mqだけなぜか16:9
-                  `https://i.ytimg.com/vi/${brief.ytId}/mqdefault.jpg`
-                }
-              />
-              <div
-                style={{
-                  paddingLeft: 32 + 96,
-                  marginTop: 160,
                   // 5xl
                   fontSize: 48,
                   lineHeight: 1,
+                  marginRight: 20,
                   fontFamily: fontMainUi,
                 }}
               >
-                {cid}
-              </div>
-              <div
-                style={{
-                  paddingLeft: 32,
-                  marginTop: 32,
-                  width: 2147483647,
-                  // 困ったことにellipsisが効かない
-                  // width: "100%",
-                  // textWrap: "nowrap",
-                  // textOverflow: "ellipsis",
-                  // overflowX: "clip",
-                  // overflowY: "visible",
-                  // 7xl
-                  fontSize: 72,
-                  lineHeight: 1,
-                  fontFamily: fontTitle,
-                }}
-              >
-                {brief.title}
-              </div>
-              <div
-                style={{
-                  paddingLeft: 32,
-                  marginTop: 24,
-                  width: 2147483647,
-                  // 6xl
-                  fontSize: 60,
-                  lineHeight: 1,
-                  fontFamily: fontTitle,
-                }}
-              >
-                {brief.composer}
-              </div>
-              <div
-                style={{
-                  display: "flex",
-                  alignItems: "baseline",
-                  paddingLeft: 32,
-                  marginTop: 24,
-                  width: 2147483647,
-                  // 6xl
-                  fontSize: 60,
-                  lineHeight: 1,
-                  fontFamily: fontTitle,
-                }}
-              >
-                <span
-                  style={{
-                    // 5xl
-                    fontSize: 48,
-                    lineHeight: 1,
-                    marginRight: 20,
-                    fontFamily: fontMainUi,
-                  }}
-                >
-                  {t("chartCreator")}:
-                </span>
-                <span>{brief.chartCreator}</span>
-              </div>
+                {t("chartCreator")}:
+              </span>
+              <span>{brief.chartCreator}</span>
             </div>
+          </div>
+        ),
+        {
+          width: 1200,
+          height: 630,
+          fonts: await Promise.all(
+            pFonts.map(async (f) => ({
+              name: f.name,
+              weight: f.weight,
+              style: f.style,
+              data: await (await f.pData).arrayBuffer(),
+            }))
           ),
-          {
-            width: 1200,
-            height: 630,
-            fonts: await Promise.all(
-              pFonts.map(async (f) => ({
-                name: f.name,
-                weight: f.weight,
-                style: f.style,
-                data: await (await f.pData).arrayBuffer(),
-              }))
-            ),
-          }
-        ).body!
+        }
       );
+      if (imRes.ok && imRes.body) {
+        return c.body(imRes.body, 200, {
+          "Content-Type": imRes.headers.get("Content-Type") || "",
+          "Cache-Control": "no-store",
+        });
+      } else {
+        console.error(imRes);
+        throw new HTTPException(500, {
+          message: "Failed to generate image",
+        });
+      }
     } else {
       let message = "";
       try {
