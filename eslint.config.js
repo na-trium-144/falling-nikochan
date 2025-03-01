3ed54eb68f32090458da7ded713d21a0eabb1f6b
k. Naka <100704180+na-trium-144@users.noreply.github.com>
2025-03-02 01:53:52 +0900

    eslintconfigをルートに移動し、設定内容を見直し (#337)
    * eslintconfigをルートに移動し、設定内容を見直し
    * mongodbを起動するスクリプトをpackage.jsonに追加

---
+++ eslint.config.js
@@ -0,0 +1,52 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import eslintConfigPrettier from "eslint-config-prettier";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const compat = new FlatCompat({
+  baseDirectory: import.meta.dirname,
+});
+
+const eslintPluginNext = compat.config({
+  extends: ["next/core-web-vitals", "next/typescript"],
+  settings: {
+    next: {
+      rootDir: "frontend/",
+    },
+  },
+  rules: {
+    "@next/next/no-html-link-for-pages": "warn",
+    "@next/next/no-img-element": "warn",
+    "jsx-a11y/alt-text": "warn",
+  },
+});
+
+const config = [
+  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  eslintConfigPrettier,
+  ...eslintPluginNext,
+  {
+    files: ["i18n/**/*"],
+    rules: {
+      "import/no-anonymous-default-export": "off",
+    },
+  },
+  {
+    files: ["route/**/*"],
+    rules: {
+      "@next/next/no-img-element": "off",
+      "jsx-a11y/alt-text": "off",
+    },
+  },
+  {
+    rules: {
+      "prefer-const": "off",
+      "@typescript-eslint/no-explicit-any": "warn",
+    },
+  },
+];
+
+export default config;
