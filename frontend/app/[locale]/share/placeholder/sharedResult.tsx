c08572267bd640528a1f250b9d75e27a8b4df190
na-trium-144 <100704180+na-trium-144@users.noreply.github.com>
2025-03-03 18:20:18 +0900

    shareページに表示するresultのレイアウトを改良

--- frontend/app/[locale]/share/placeholder/sharedResult.tsx
+++ frontend/app/[locale]/share/placeholder/sharedResult.tsx
@@ -1,9 +1,11 @@
 import { Box } from "@/common/box";
+import { levelColors } from "@/common/levelColors";
 import { JudgeIcon } from "@/play/statusBox";
 import {
   baseScoreRate,
   bigScoreRate,
   chainScoreRate,
+  levelTypes,
   rankStr,
   ResultParams,
 } from "@falling-nikochan/chart";
@@ -22,77 +24,103 @@ export function SharedResultBox(props: Props) {
     setResultDate(new Date(props.result.date).toLocaleDateString());
   }, [props.result.date]);
   return (
-    <Box className="mx-auto p-4 ">
+    <Box className="mx-auto py-4 px-6 mt-4">
       <p className="text-lg font-title font-bold text-center ">
         &lt; {th("sharedResult")} &gt;
       </p>
-      <p className="text-center text-slate-500 dark:text-stone-400 ">
-        ({resultDate})
+      <p className="text-center ">
+        {props.result.lvName && (
+          <span className="font-title mr-2">{props.result.lvName}</span>
+        )}
+        <span className={"mr-2 " + levelColors[props.result.lvType]}>
+          <span className="text-sm">{levelTypes[props.result.lvType]}-</span>
+          <span className="text-lg">{props.result.lvDifficulty}</span>
+        </span>
+        <span className="text-slate-500 dark:text-stone-400 ">
+          ({resultDate})
+        </span>
       </p>
-      <div className="flex flex-row space-x-6 ">
-        <div className="flex flex-col">
-          {(
-            [
-              ["baseScore", props.result.baseScore100],
-              ["chainBonus", props.result.chainScore100],
-              ["bigNoteBonus", props.result.bigScore100],
-            ] as const
-          ).map(([name, score100], i) => (
-            <p key={i} className="flex flex-row w-full items-baseline ">
-              <span className="flex-1 text-sm ">{t(name)}:</span>
-              <span className="text-2xl">{Math.floor(score100 / 100)}</span>
+      <div
+        className={
+          "flex flex-col items-center gap-2 " +
+          "main-wide:flex-row main-wide:gap-6 "
+        }
+      >
+        <div
+          className={
+            "flex flex-col items-center gap-2 " +
+            "share-wide2:flex-row share-wide2:gap-6"
+          }
+        >
+          <div className="flex flex-col w-48">
+            {(
+              [
+                ["baseScore", props.result.baseScore100],
+                ["chainBonus", props.result.chainScore100],
+                ["bigNoteBonus", props.result.bigScore100],
+              ] as const
+            ).map(([name, score100], i) => (
+              <p key={i} className="flex flex-row w-full items-baseline ">
+                <span className="flex-1 text-sm ">{t(name)}:</span>
+                <span className="text-2xl">{Math.floor(score100 / 100)}</span>
+                <span className="">.</span>
+                <span className="text-left w-5 ">
+                  {(score100 % 100).toString().padStart(2, "0")}
+                </span>
+              </p>
+            ))}
+            <div className="mt-1 border-b border-slate-800 dark:border-stone-300" />
+            <p className="flex flex-row w-full items-baseline ">
+              <span className="flex-1 text-sm ">{t("totalScore")}:</span>
+              <span className="text-2xl">
+                {Math.floor(props.result.score100 / 100)}
+              </span>
               <span className="">.</span>
               <span className="text-left w-5 ">
-                {(score100 % 100).toString().padStart(2, "0")}
+                {(props.result.score100 % 100).toString().padStart(2, "0")}
               </span>
             </p>
-          ))}
-          <div className="mt-1 border-b border-slate-800 dark:border-stone-300" />
-          <p className="flex flex-row w-full items-baseline ">
-            <span className="flex-1 text-sm ">{t("totalScore")}:</span>
-            <span className="text-2xl">
-              {Math.floor(props.result.score100 / 100)}
-            </span>
-            <span className="">.</span>
-            <span className="text-left w-5 ">
-              {(props.result.score100 % 100).toString().padStart(2, "0")}
-            </span>
-          </p>
-        </div>
-        <div className="flex-none flex flex-col justify-center items-center ">
-          <div>
-            <span className="mr-2">{t("rank")}:</span>
-            <span className="text-3xl">
-              {rankStr(props.result.score100 / 100)}
-            </span>
           </div>
-          {props.result.chainScore100 === chainScoreRate * 100 ? (
-            <div className="text-xl">
-              <span className="">
-                {props.result.baseScore100 === baseScoreRate * 100
-                  ? t("perfect")
-                  : t("full")}
+          <div className="w-40 flex flex-col justify-center items-center ">
+            <div>
+              <span className="mr-2">{t("rank")}:</span>
+              <span className="text-3xl">
+                {rankStr(props.result.score100 / 100)}
               </span>
-              {props.result.bigScore100 === bigScoreRate * 100 && (
-                <span className="font-bold">+</span>
-              )}
-              <span>!</span>
             </div>
-          ) : null}
+            {props.result.chainScore100 === chainScoreRate * 100 ? (
+              <div className="text-xl">
+                <span className="">
+                  {props.result.baseScore100 === baseScoreRate * 100
+                    ? t("perfect")
+                    : t("full")}
+                </span>
+                {props.result.bigScore100 === bigScoreRate * 100 && (
+                  <span className="font-bold">+</span>
+                )}
+                <span>!</span>
+              </div>
+            ) : null}
+          </div>
         </div>
-        <div className="flex flex-col justify-center ">
+        <div
+          className={
+            "w-32 flex flex-col justify-center " +
+            "text-slate-500 dark:text-stone-400 "
+          }
+        >
           {["good", "ok", "bad", "miss"].map((name, ji) => (
             <div key={ji} className="flex flex-row items-baseline ">
-              <span className="inline-block w-5 translate-y-1">
+              <span className="inline-block text-sm w-4 translate-y-0.5">
                 <JudgeIcon index={ji} />
               </span>
-              <span className="flex-1 text-sm ">{ts(name)}</span>
-              <span className="text-lg/6 ">{props.result.judgeCount[ji]}</span>
+              <span className="flex-1 text-xs ">{ts(name)}</span>
+              <span className="text-base ">{props.result.judgeCount[ji]}</span>
             </div>
           ))}
           <div className="flex flex-row items-baseline ">
-            <span className="flex-1 text-sm ">{ts("big")}</span>
-            <span className="text-lg/6 ">{props.result.bigCount}</span>
+            <span className="flex-1 text-xs ">{ts("big")}</span>
+            <span className="text-base ">{props.result.bigCount}</span>
           </div>
         </div>
       </div>
