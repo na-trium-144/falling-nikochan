1d40c785ab0eae19994133be31839d6ae845f091
na-trium-144 <100704180+na-trium-144@users.noreply.github.com>
2025-03-30 22:36:23 +0900

    iOSでページを開けませんになる問題が解決

--- route/src/redirect.ts
+++ route/src/redirect.ts
@@ -1,6 +1,8 @@
 import { Context, Hono } from "hono";
 import { Bindings, languageDetector } from "./env.js";
 
+// 307リダイレクトの際にoriginを指定しないと、iOSのPWA内でページを開けませんという画面になる。なんでやねん。
+
 const redirectApp = (config?: {
   languageDetector?: (c: Context, next: () => Promise<void>) => Promise<void>;
 }) =>
@@ -9,13 +11,13 @@ const redirectApp = (config?: {
     .get("/edit/:cid", (c) => {
       // deprecated (used until ver6.15)
       const cid = c.req.param("cid");
-      return c.redirect(`/edit?cid=${cid}`, 301);
+      return c.redirect(`${new URL(c.req.url).origin}/edit?cid=${cid}`, 301);
     })
     .on("get", ["/", "/edit", "/main/*", "/play"], (c) => {
       const lang = c.get("language");
       const params = new URLSearchParams(new URL(c.req.url).search);
       return c.redirect(
-        `/${lang}${c.req.path}${params ? "?" + params : ""}`,
+        `${new URL(c.req.url).origin}/${lang}${c.req.path}${params ? "?" + params : ""}`,
         307,
       );
     });
