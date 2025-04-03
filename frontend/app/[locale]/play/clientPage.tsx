e9c33f422b2e4ecdd8d4702a1902c2b40a0f55d0
k. Naka <100704180+na-trium-144@users.noreply.github.com>
2025-04-03 17:08:44 +0900

    画面の更新を60fps以下に制限 (#445)
    * 画面の更新を60fps以下に制限
    
    * フレームレート制限オプションを追加
    
    * fps計測を改善
    
    * selectの矢印にpointer-events-none指定を追加
    
    * lintエラー修正
    
    * ver10.9
    
    * 最小値を15fpsに変更

--- frontend/app/[locale]/play/clientPage.tsx
+++ frontend/app/[locale]/play/clientPage.tsx
@@ -284,7 +284,7 @@ function Play(props: Props) {
     !statusHide;
   const mainWindowSpace = useResizeDetector();
   const readySmall =
-    !!mainWindowSpace.height && mainWindowSpace.height < 30 * rem;
+    !!mainWindowSpace.height && mainWindowSpace.height < 36 * rem;
 
   const [bestScoreState, setBestScoreState] = useState<number>(0);
   const reloadBestScore = useCallback(() => {
@@ -361,7 +361,37 @@ function Play(props: Props) {
     lateTimes,
   } = useGameLogic(getCurrentTimeSec, auto, userOffset, playSE);
 
-  const [fps, setFps] = useState<number>(0);
+  const [fps, setFps_] = useState<number>(0);
+  // nフレームに1回画面を更新する
+  // フレームレートが60を超える端末の場合に、60を超えないように自動的に設定して制限する
+  const [frameDrop, setFrameDrop_] = useState<number | null>(null);
+  const [maxFPS, setMaxFPS] = useState<number>(0);
+  const fpsStable = useRef<number>(0);
+  const setFps = useCallback(
+    (fps: number) => {
+      setFps_(fps);
+      if (frameDrop === null && fps > 0) {
+        // fps計測が安定するまで待つ
+        if (fps <= maxFPS) {
+          fpsStable.current++;
+          if (fpsStable.current > maxFPS) {
+            setFrameDrop_(
+              Number(localStorage.getItem("frameDrop") || 0) ||
+                Math.max(1, Math.ceil(fps / 60 - 0.3)), // 2捨3入
+            );
+          }
+        } else {
+          fpsStable.current = 0;
+          setMaxFPS(fps);
+        }
+      }
+    },
+    [frameDrop, maxFPS],
+  );
+  const setFrameDrop = useCallback((v: number) => {
+    setFrameDrop_(v);
+    localStorage.setItem("frameDrop", v.toString());
+  }, []);
 
   useEffect(() => {
     if (ref.current) {
@@ -714,6 +744,7 @@ function Play(props: Props) {
         </div>
         <div className={"relative flex-1"} ref={mainWindowSpace.ref}>
           <FallingWindow
+            frameDrop={frameDrop || 1}
             className="absolute inset-0"
             notes={notesAll}
             getCurrentTimeSec={getCurrentTimeSec}
@@ -730,15 +761,8 @@ function Play(props: Props) {
                 : "opacity-0 translate-y-[-300px]")
             }
           >
-            <ScoreDisp
-              score={score}
-              best={bestScoreState}
-              auto={auto}
-            />
-            <ChainDisp
-              chain={chain}
-              fc={judgeCount[2] + judgeCount[3] === 0}
-            />
+            <ScoreDisp score={score} best={bestScoreState} auto={auto} />
+            <ChainDisp chain={chain} fc={judgeCount[2] + judgeCount[3] === 0} />
             <button
               className={
                 "absolute rounded-full cursor-pointer leading-1 " +
@@ -790,6 +814,9 @@ function Play(props: Props) {
               enableSE={enableSE}
               setEnableSE={setEnableSE}
               audioLatency={audioLatency}
+              maxFPS={maxFPS}
+              frameDrop={frameDrop}
+              setFrameDrop={setFrameDrop}
               editing={editing}
               lateTimes={lateTimes.current}
               small={readySmall}
