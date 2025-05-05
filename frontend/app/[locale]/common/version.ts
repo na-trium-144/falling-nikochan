56e8f620c09c65b205cbbd0ae2099b13e712602b
k. Naka <100704180+na-trium-144@users.noreply.github.com>
2025-05-06 01:41:41 +0900

    play中の画像アセットを音符1個ごとにfetchしないようbase64のdataURLに変更 (#555)
    ver11.35

--- frontend/app/[locale]/common/version.ts
+++ frontend/app/[locale]/common/version.ts
@@ -1,6 +1,6 @@
 const versionKey = "lastVisited";
 const latestChangelogMajor = 11;
-const latestChangelogMinor = 30;
+const latestChangelogMinor = 35;
 export function lastVisitedOld(): boolean {
   try {
     const lastVisited = localStorage.getItem(versionKey);
