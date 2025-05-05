9db24855b3b9ea3b097e649dbacc93eb56533308
k. Naka <100704180+na-trium-144@users.noreply.github.com>
2025-05-06 04:12:44 +0900

    themecolorの両方にmediaクエリ追加(自動darkモードのテーマカラーが反映されていなかったのを修正) (#556)

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -3,15 +3,9 @@ import "@fontsource/kaisei-opti/japanese-400.css";
 import "@fontsource/noto-sans/400.css";
 import "@fontsource/noto-sans-jp/japanese-400.css";
 import "@/globals.css";
-import type { Viewport } from "next";
-import { themeColorDark, themeColorLight } from "@/metadata";
+import { initViewport } from "@/metadata";
 
-export const viewport: Viewport = {
-  themeColor: [
-    { color: themeColorLight },
-    { media: "(prefers-color-scheme: dark)", color: themeColorDark },
-  ],
-};
+export const viewport = initViewport();
 
 export default async function RootLayout({
   children,
