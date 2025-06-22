cd3c2780f72fd559c7efe14d980f961bae3247f8
k. Naka <100704180+na-trium-144@users.noreply.github.com>
2025-06-22 18:20:57 +0900

    練習オプション (再生開始位置変更、再生速度変更) (#658)
    ver12.30
    * 再生開始位置と再生速度を変更するオプションを追加
    * オプション画面のスクロールの挙動を修正

--- frontend/app/[locale]/ogTemplate/share/clientPage.tsx
+++ frontend/app/[locale]/ogTemplate/share/clientPage.tsx
@@ -105,6 +105,7 @@ export default function OGTemplate() {
           getCurrentTimeSec={() => undefined}
           playing={false}
           bpmChanges={[]}
+          playbackRate={1}
         />
         <div className="scale-150 absolute w-full h-full bottom-0 left-0 origin-bottom-left">
           <BPMSign
@@ -112,6 +113,7 @@ export default function OGTemplate() {
             chartSeq={null}
             getCurrentTimeSec={() => undefined}
             hasExplicitSpeedChange={false}
+            playbackRate={1}
           />
         </div>
       </div>
