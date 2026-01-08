6532c5011eb798b261bd695cd216d7ea012409a0
na-trium-144 <100704180+na-trium-144@users.noreply.github.com>
2026-01-09 02:19:10 +0900

    譜面編集ヘルプのインデントを修正

--- frontend/app/[locale]/edit/guideMain.tsx
+++ frontend/app/[locale]/edit/guideMain.tsx
@@ -31,9 +31,9 @@ export function GuideMain(props: Props) {
           onClickBefore={() => props.setIndex(props.index - 1)}
           onClickAfter={() => props.setIndex(props.index + 1)}
         />
-        <ul className="list-inside list-disc-as-text flex-1 ">
+        <div className="flex-1">
           {props.content}
-        </ul>
+        </div>
         <p className="w-max m-auto">
           <Button
             text={props.index === 1 ? t("agreeClose") : t("close")}
