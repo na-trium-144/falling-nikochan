9a0d02e80216d82fb9270f6aaea8619bcd6a2683
na-trium-144 <100704180+na-trium-144@users.noreply.github.com>
2026-01-11 16:13:17 +0900

    channelTitleが長い場合の表示を修正

--- frontend/app/[locale]/share/placeholder/shareBox.tsx
+++ frontend/app/[locale]/share/placeholder/shareBox.tsx
@@ -148,6 +148,7 @@ export function ShareBox(props: Props) {
                 <p
                   className={clsx(
                     "font-title text-xs main-wide:text-sm",
+                    "overflow-hidden text-nowrap text-ellipsis",
                     defaultThemeStyle
                   )}
                 >
