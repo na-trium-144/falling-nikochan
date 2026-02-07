052ba83ed63f0dd5b1b58ffac2fe511950b19f0a
na-trium-144 <100704180+na-trium-144@users.noreply.github.com>
2026-02-07 17:57:40 +0900

    progressbar

--- frontend/app/[locale]/common/progressBar.tsx
+++ frontend/app/[locale]/common/progressBar.tsx
@@ -8,15 +8,9 @@ interface Props {
 }
 export default function ProgressBar(props: Props) {
   return (
-    <div
-      className={clsx(
-        "relative h-1 overflow-hidden rounded-full shadow bg-black/25 dark:bg-white/25",
-        props.className
-      )}
-    >
+    <div className={clsx("fn-progressbar", props.className)}>
       <div
         className={clsx(
-          "absolute inset-y-0 rounded-full",
           props.fixedColor !== undefined
             ? props.fixedColor
             : props.value < 0.5
