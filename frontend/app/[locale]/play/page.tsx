4f92997752d2fd4a15ac5252bb6c3b09d3fa11b5
na-trium-144 <100704180+na-trium-144@users.noreply.github.com>
2026-02-19 18:47:27 +0900

    playページとeditページでのみキーボードによるボタン操作を無効化

--- frontend/app/[locale]/play/page.tsx
+++ frontend/app/[locale]/play/page.tsx
@@ -1,6 +1,7 @@
 import { getTranslations } from "@falling-nikochan/i18n/dynamic.js";
 import { InitPlay } from "./clientPage.js";
 import { initMetadata, MetadataProps } from "@/metadata.js";
+import { ButtonKeyDisabler } from "@/common/button.jsx";
 
 export async function generateMetadata({ params }: MetadataProps) {
   const t = await getTranslations(params, "play");
@@ -9,5 +10,9 @@ export async function generateMetadata({ params }: MetadataProps) {
 // pageTitle(cid, brief) or `Not Found (ID: ${cid})`
 
 export default async function Page({ params }: MetadataProps) {
-  return <InitPlay locale={(await params).locale} />;
+  return (
+    <ButtonKeyDisabler>
+      <InitPlay locale={(await params).locale} />
+    </ButtonKeyDisabler>
+  );
 }
