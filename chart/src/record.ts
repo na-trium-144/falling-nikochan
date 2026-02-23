d53f6370a8f33e7ea66ad05c2e4c00420095aabb
na-trium-144 <100704180+na-trium-144@users.noreply.github.com>
2026-02-23 12:28:22 +0900

    recordのスコア計算を修正

--- chart/src/record.ts
+++ chart/src/record.ts
@@ -49,7 +49,8 @@ export const RecordPostSchema = () =>
               record.baseScore -
               record.chainScore -
               record.bigScore
-          ) < Number.EPSILON
+          ) <
+          Number.EPSILON * record.score * 4
         );
       } else {
         return false;
