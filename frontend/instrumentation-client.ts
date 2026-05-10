1fc78e4d27a4f54dc12b78e657eedcf6f6530675
na-trium-144 <100704180+na-trium-144@users.noreply.github.com>
2026-05-10 15:26:56 +0900

    piiを明示的にfalseに

--- frontend/instrumentation-client.ts
+++ frontend/instrumentation-client.ts
@@ -1,8 +1,11 @@
 import * as Sentry from "@sentry/nextjs";
 
+// Sentryの初期化箇所はここ以外に app/[locale]/edit/luaExecWorker.ts にもあるので注意！(そちらも同様に編集すること)
+
 Sentry.init({
   dsn: process.env.SENTRY_DSN,
   release: process.env.SENTRY_RELEASE,
+  sendDefaultPii: false,
   enabled: process.env.NODE_ENV !== "development",
   integrations: [Sentry.extraErrorDataIntegration({ depth: 10 })],
 });
