0831cfbb1c796ac78f67850d3cb6b6778636e458
k. Naka <100704180+na-trium-144@users.noreply.github.com>
2026-05-15 02:44:29 +0900

    chore sentry config (#1131)
    * widenClientFileUpload
    
    * environmentをTITLE_SUFFIX環境変数から指定
    
    * ver15.12

--- frontend/app/[locale]/edit/luaExecWorker.ts
+++ frontend/app/[locale]/edit/luaExecWorker.ts
@@ -5,6 +5,7 @@ import * as Sentry from "@sentry/browser";
 Sentry.init({
   dsn: process.env.SENTRY_DSN,
   release: process.env.SENTRY_RELEASE,
+  environment: (process.env.TITLE_SUFFIX || "production").toLowerCase(),
   sendDefaultPii: false,
   enabled: process.env.NODE_ENV !== "development",
   integrations: [Sentry.extraErrorDataIntegration({ depth: 10 })],
