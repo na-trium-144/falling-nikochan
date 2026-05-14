ee34b0e80c3b6fbb1a94857a4801491809bd0321
na-trium-144 <100704180+na-trium-144@users.noreply.github.com>
2026-05-14 12:51:08 +0900

    エラーページにeventIdの表示を追加

--- frontend/app/[locale]/error.tsx
+++ frontend/app/[locale]/error.tsx
@@ -1,6 +1,6 @@
 "use client"; // Error boundaries must be Client Components
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import * as Sentry from "@sentry/nextjs";
 import { useTranslations } from "next-intl";
 import { CenterBox } from "@/common/box";
@@ -16,13 +16,14 @@ interface ErrorProps {
 }
 export default function ClientErrorPage(props: ErrorProps) {
   const t = useTranslations("error.errorPage");
+  const [eventId, setEventId] = useState<string>();
   useEffect(() => {
-    Sentry.captureException(props.error);
+    setEventId(Sentry.captureException(props.error));
   }, [props.error]);
   return (
     <CenterBox scrollableY>
       <h4 className="fn-heading-box">{t("title")}</h4>
-      <ErrorMessage error={props.error} />
+      <ErrorMessage error={props.error} eventId={eventId} />
       <LinksOnError />
       <GoHomeButton />
     </CenterBox>
