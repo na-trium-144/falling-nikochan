685901e0165a1ef42220e582e2b61a3698a24d8b
na-trium-144 <100704180+na-trium-144@users.noreply.github.com>
2026-05-15 22:28:03 +0900

    aboutとquery保持のテストを追加

--- route/tests/redirect.spec.ts
+++ route/tests/redirect.spec.ts
@@ -18,6 +18,14 @@ describe("redirectApp", () => {
     expect(location.pathname).to.equal("/edit");
     expect(location.searchParams.get("cid")).to.equal("123456");
   });
+  test("should redirect deprecated /edit/:cid to /edit?cid=:cid with other query preserved", async () => {
+    const res = await app.request("http://example.com/edit/123456?foo=bar");
+    expect(res.status).to.equal(301);
+    const location = new URL(res.headers.get("Location")!);
+    expect(location.pathname).to.equal("/edit");
+    expect(location.searchParams.get("cid")).to.equal("123456");
+    expect(location.searchParams.get("foo")).to.equal("bar");
+  });
 
   test("should redirect deprecated /:lang/main/:sort to /:lang/main/play?sort=:sort", async () => {
     const res = await app.request("http://example.com/ja/main/latest");
@@ -26,8 +34,51 @@ describe("redirectApp", () => {
     expect(location.pathname).to.equal("/ja/main/play");
     expect(location.searchParams.get("sort")).to.equal("latest");
   });
+  test("should redirect deprecated /:lang/main/:sort to /:lang/main/play?sort=:sort with other query preserved", async () => {
+    const res = await app.request("http://example.com/ja/main/latest?foo=bar");
+    expect(res.status).to.equal(301);
+    const location = new URL(res.headers.get("Location")!);
+    expect(location.pathname).to.equal("/ja/main/play");
+    expect(location.searchParams.get("sort")).to.equal("latest");
+    expect(location.searchParams.get("foo")).to.equal("bar");
+  });
 
-  test("should 307 redirect play route with language prefix and query", async () => {
+  test("should redirect deprecated /:lang/main/about/1 to /:lang with anchor", async () => {
+    const res = await app.request("http://example.com/ja/main/about/1");
+    expect(res.status).to.equal(301);
+    const location = new URL(res.headers.get("Location")!);
+    expect(location.pathname).to.equal("/ja");
+    expect(location.hash).to.equal("#feature-play");
+  });
+  test("should redirect deprecated /:lang/main/about/4 to /:lang/main/about", async () => {
+    const res = await app.request("http://example.com/ja/main/about/4");
+    expect(res.status).to.equal(301);
+    const location = new URL(res.headers.get("Location")!);
+    expect(location.pathname).to.equal("/ja/main/about");
+  });
+  test("should redirect deprecated /:lang/main/about/1 to /:lang with anchor with other query preserved", async () => {
+    const res = await app.request("http://example.com/ja/main/about/1?foo=bar");
+    expect(res.status).to.equal(301);
+    const location = new URL(res.headers.get("Location")!);
+    expect(location.pathname).to.equal("/ja");
+    expect(location.hash).to.equal("#feature-play");
+    expect(location.searchParams.get("foo")).to.equal("bar");
+  });
+  test("should redirect deprecated /:lang/main/about/4 to /:lang/main/about with other query preserved", async () => {
+    const res = await app.request("http://example.com/ja/main/about/4?foo=bar");
+    expect(res.status).to.equal(301);
+    const location = new URL(res.headers.get("Location")!);
+    expect(location.pathname).to.equal("/ja/main/about");
+    expect(location.searchParams.get("foo")).to.equal("bar");
+  });
+
+  test("should 307 redirect play route with language prefix", async () => {
+    const res = await app.request("http://example.com/play");
+    expect(res.status).to.equal(307);
+    const location = new URL(res.headers.get("Location")!);
+    expect(location.pathname).to.equal("/ja/play");
+  });
+  test("should 307 redirect play route with language prefix with query preserved", async () => {
     const res = await app.request("http://example.com/play?cid=100000");
     expect(res.status).to.equal(307);
     const location = new URL(res.headers.get("Location")!);
