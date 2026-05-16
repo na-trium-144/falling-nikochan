2507e279fd8d9a791514ab2c5fc035cb396850d7
na-trium-144 <100704180+na-trium-144@users.noreply.github.com>
2026-05-16 22:20:48 +0900

    fix

--- frontend/app/[locale]/play/clientPage.tsx
+++ frontend/app/[locale]/play/clientPage.tsx
@@ -691,26 +691,31 @@ function Play(props: Props) {
             playbackRate === 1 &&
             chartBrief?.levels.at(lvIndex)
           ) {
-            fetch(process.env.BACKEND_PREFIX + `/api/record/${cid}`, {
-              method: "POST",
-              headers: { "Content-Type": "application/json" },
-              body: JSON.stringify({
-                lvHash: chartBrief.levels[lvIndex].hash,
-                auto,
-                score,
-                baseScore,
-                chainScore,
-                bigScore,
-                fc: chainScore === chainScoreRate,
-                fb: bigScore === bigScoreRate,
-                editing,
-                factor: updateRecordFactor(
-                  cid,
-                  chartBrief.levels[lvIndex].hash,
-                  auto
-                ),
-              } satisfies RecordPost),
-            }).catch(() => undefined);
+            try {
+              const factor = updateRecordFactor(
+                cid,
+                chartBrief.levels[lvIndex].hash,
+                auto
+              );
+              fetch(process.env.BACKEND_PREFIX + `/api/record/${cid}`, {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({
+                  lvHash: chartBrief.levels[lvIndex].hash,
+                  auto,
+                  score,
+                  baseScore,
+                  chainScore,
+                  bigScore,
+                  fc: chainScore === chainScoreRate,
+                  fb: bigScore === bigScoreRate,
+                  editing,
+                  factor,
+                } satisfies RecordPost),
+              }).catch(() => undefined);
+            } catch {
+              // ignore errors from updateRecordFactor
+            }
           }
           setResultDate(newResultDate);
           setExitable((ex) =>
