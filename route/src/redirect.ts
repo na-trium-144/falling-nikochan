2a3c6a3017564356e9d9ee5bdb331ba5affce4d5
na-trium-144 <100704180+na-trium-144@users.noreply.github.com>
2026-07-27 03:12:51 +0900

    リダイレクトのパスを正規化
    `/` -> `/ja/?` になっていたのを `/ja` になるよう修正

--- route/src/redirect.ts
+++ route/src/redirect.ts
@@ -39,7 +39,7 @@ const redirectApp = (config: {
     })
     .get("/:lang/main/about/:page{[1-5]}", (c) => {
       // deprecated (used until ver15.12)
-      const q = new URLSearchParams(new URL(c.req.url).search);
+      const search = new URL(c.req.url).search;
       const lang = c.req.param("lang");
       const page = Number(c.req.param("page"));
       switch (page) {
@@ -47,20 +47,20 @@ const redirectApp = (config: {
         case 2:
           c.header("cache-control", immutable());
           return c.redirect(
-            new URL(`/${lang}?${q}#feature-play`, backendOrigin(c)),
+            new URL(`/${lang}${search}#feature-play`, backendOrigin(c)),
             301
           );
         case 3:
           c.header("cache-control", immutable());
           return c.redirect(
-            new URL(`/${lang}?${q}#feature-edit`, backendOrigin(c)),
+            new URL(`/${lang}${search}#feature-edit`, backendOrigin(c)),
             301
           );
         case 4:
         case 5:
           c.header("cache-control", immutable());
           return c.redirect(
-            new URL(`/${lang}/main/about?${q}`, backendOrigin(c)),
+            new URL(`/${lang}/main/about${search}`, backendOrigin(c)),
             301
           );
         default:
@@ -68,12 +68,10 @@ const redirectApp = (config: {
       }
     })
     .on("get", ["/", "/edit", "/main/*", "/play"], async (c) => {
-      const q = new URLSearchParams(new URL(c.req.url).search);
+      const search = new URL(c.req.url).search;
+      const path = c.req.path === "/" ? "" : c.req.path;
       const lang = c.get("language");
-      const redirected = new URL(
-        `/${lang}${c.req.path}?${q}`,
-        backendOrigin(c)
-      );
+      const redirected = new URL(`/${lang}${path}${search}`, backendOrigin(c));
       if (isbot(c.req.header("User-Agent"))) {
         // crawlerに対してはリダイレクトのレスポンスを返す代わりにリダイレクト先のページを直接返す
         const res = await config.fetchStatic(env(c), new URL(redirected));
