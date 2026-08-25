6d3fa25511d1f251462b276d8be99129b2117c19
na-trium-144 <100704180+na-trium-144@users.noreply.github.com>
2026-08-26 02:06:27 +0900

    背景画像を更新

--- frontend/app/[locale]/ogTemplate/title/clientPage.tsx
+++ frontend/app/[locale]/ogTemplate/title/clientPage.tsx
@@ -8,7 +8,7 @@ import {
   IrasutoyaLikeBgInner,
   IrasutoyaLikeGrassInner,
 } from "@/common/irasutoyaLike";
-import { useEffect } from "react";
+import { useEffect, useMemo, useState } from "react";
 
 export default function OGTemplate() {
   // 1rem = 16px の環境でのみ正常に表示される
@@ -31,60 +31,93 @@ export default function OGTemplate() {
       window.removeEventListener("keydown", keydown);
     };
   }, []);
+
+  const [mode, setMode] = useState<null | string>(null);
+  useEffect(() => {
+    setMode(new URLSearchParams(window.location.search).get("mode"));
+  }, []);
+
+  const options = useMemo(
+    () =>
+      mode === "twitter"
+        ? {
+            width: 1200,
+            height: 400,
+            titleHeight: 100,
+            titleScale: "288%",
+            bpmSign: false,
+            slime: false,
+          }
+        : {
+            width: 1200,
+            height: 630,
+            titleHeight: 136,
+            titleScale: "320%",
+            bpmSign: true,
+            slime: true,
+          },
+    [mode]
+  );
+
   return (
     <div
       className="absolute isolate flex flex-col overflow-clip "
-      style={{ width: 1200, height: 630 }}
+      style={{ width: options.width, height: options.height }}
     >
       <IrasutoyaLikeBgInner
-        screenWidth={1200}
-        screenHeight={630}
+        screenWidth={options.width}
+        screenHeight={options.height}
         fixedSeed
         className="absolute"
       />
       <Title
-        className="absolute top-0 inset-x-0 h-34 scale-320 origin-top "
+        className="absolute top-0 inset-x-0 origin-top "
+        style={{ height: options.titleHeight, scale: options.titleScale }}
         anim={false}
       />
       <div className="absolute bottom-0 w-full h-6">
         <IrasutoyaLikeGrassInner
           rem={16}
-          screenWidth={1200}
-          screenHeight={630}
+          screenWidth={options.width}
+          screenHeight={options.height}
           height={2.5 * 16}
           fixedSeed
           classNameFar="absolute"
           classNameNear="absolute"
         />
-        <RhythmicalSlime
-          className="z-14 absolute scale-150 origin-bottom-right "
-          style={{
-            bottom: "100%",
-            right: "1rem",
-          }}
-          signature={[
-            {
-              step: stepZero(),
-              offset: stepZero(),
-              barNum: 0,
-              bars: [[4, 4, 4, 4]],
-            },
-          ]}
-          getCurrentTimeSec={() => undefined}
-          playing={false}
-          bpmChanges={[]}
-          playbackRate={1}
-          startsJumping={null}
-        />
-        <div className="z-13 scale-150 absolute w-full h-full bottom-0 left-0 translate-y-4 origin-bottom-left">
-          <BPMSign
-            chartPlaying={false}
-            chartSeq={null}
+        {options.slime && (
+          <RhythmicalSlime
+            className="z-14 absolute scale-150 origin-bottom-right "
+            style={{
+              bottom: "100%",
+              right: "1rem",
+            }}
+            signature={[
+              {
+                step: stepZero(),
+                offset: stepZero(),
+                barNum: 0,
+                bars: [[4, 4, 4, 4]],
+              },
+            ]}
             getCurrentTimeSec={() => undefined}
-            hasExplicitSpeedChange={false}
+            playing={false}
+            bpmChanges={[]}
             playbackRate={1}
+            startsJumping={null}
           />
-        </div>
+        )}
+        {options.bpmSign && (
+          <div className="z-13 scale-150 absolute w-full h-full bottom-0 left-0 translate-y-4 origin-bottom-left">
+            <BPMSign
+              chartPlaying={false}
+              chartSeq={null}
+              getCurrentTimeSec={() => undefined}
+              hasExplicitSpeedChange={false}
+              playbackRate={1}
+            />
+          </div>
+        )}
       </div>
     </div>
   );
