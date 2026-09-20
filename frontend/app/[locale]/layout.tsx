38a0d17999bf2a4c02e3747396ab5e9434392ba1
na-trium-144 <100704180+na-trium-144@users.noreply.github.com>
2026-09-20 22:16:59 +0900

    iOS26用に上下端にダミーの固定ブロックを置く

--- frontend/app/[locale]/layout.tsx
+++ frontend/app/[locale]/layout.tsx
@@ -41,6 +41,8 @@ export default async function RootLayout({
             </ChangeLogProvider>
           </IntlProvider>
         </ThemeProvider>
+        <div className="fn-ios-dummy-block fn-ios-dummy-block-top" />
+        <div className="fn-ios-dummy-block fn-ios-dummy-block-bottom" />
         {process.env.NODE_ENV === "development" && (
           <>
             <div className="fn-dev-safe-area fn-dev-sai-t" />
